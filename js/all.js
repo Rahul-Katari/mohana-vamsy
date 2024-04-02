59837ab61059868f21c12725b096635b56475fa8
rahul.katari <rahulkatari99@gmail.com>
2024-04-02 19:30:21 +0530

    changes

--- js/all.js
+++
@@ -1,5 +0,0 @@
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
-// import "https://code.jquery.com/jquery-2.2.4.min.js";
-import "./jquery.min.js";
-import "./main.js";
-// import "https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js";
--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-  // Load navbar
-  $("#navbar").load("navbar.html");
-  // Load footer
-  $("#footer").load("footer.html");
-});
